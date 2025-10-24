5c0266d3650918da1fdbbf438fb48b691b3ab7d1
eldar130 <veldarv@gmail.com>
2025-10-24 17:37:08 +0100

    player and enemy animation when moving
    player and enemy animations play in the directions of movement, but player animations switch when going diagonal and there is an error loading animations for enemy that doesn't seem to break other code

---
+++ Assets/Characters/Enemy/EnemyMovmentAnimation.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+public class EnemyControls : MonoBehaviour
+{
+    [SerializeField] private Animator animator;
+    [SerializeField] private Rigidbody2D rb;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        if (animator == null || rb == null)
+        {
+            Debug.LogError("Missing Animator or Rigidbody2D on " + gameObject.name);
+            return;
+        }
+        Vector2 direction = rb.linearVelocity;
+        //moving right
+        if (direction.x > 0)
+        {
+            animator.SetInteger("direction", 2);
+            //Debug.Log("moving right");
+        }
+        //moving left
+        else if (direction.x < 0)
+        {
+            animator.SetInteger("direction", 4);
+            //Debug.Log("moving left");
+        }
+        //moving up
+        else if (direction.y > 0)
+        {
+            animator.SetInteger("direction", 4);
+            //Debug.Log("moving up");
+        }
+        //moving down
+        else if (direction.y < 0)
+        {
+            animator.SetInteger("direction", 1);
+            //Debug.Log("moving down");
+        }
+        else
+        {
+            //Debug.Log("not moving");
+        }
+    }
+}
--- Assets/Characters/Player/PlayerControls.cs
+++ Assets/Characters/Player/PlayerControls.cs
@@ -1,11 +1,13 @@
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem.Controls;
 
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float speed = 3f;
     private float horizontal;
     private float vertical;
-    private Animator animator;
+    [SerializeField] private Animator animator;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -21,39 +23,41 @@ public class PlayerMovement : MonoBehaviour
         vertical = Input.GetAxis("Vertical");
         Vector2 movement = new Vector2(horizontal, vertical);
         transform.Translate(movement * speed * Time.deltaTime);
-
+        // W key
         if (Input.GetKeyDown(KeyCode.W))
         {
-            animator.SetTrigger("Player_running_back");
+            animator.SetBool("IsRunningBack", true);
         }
         if (Input.GetKeyUp(KeyCode.W))
         {
-            animator.SetTrigger("Player_idle_back");
+            animator.SetBool("IsRunningBack", false);
         }
-
+        // A key
         if (Input.GetKeyDown(KeyCode.A))
         {
-            if (Input.GetKeyUp(KeyCode.A))
-            {
-
-            }
+            animator.SetBool("IsRunningLeft", true);
         }
-
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            animator.SetBool("IsRunningLeft", false);
+        }
+        // S key
         if (Input.GetKeyDown(KeyCode.S))
         {
-            
+            animator.SetBool("IsRunningFront", true);
         }
         if (Input.GetKeyUp(KeyCode.S))
         {
-            
+            animator.SetBool("IsRunningFront", false);
         }
-
+        // D key
         if (Input.GetKeyDown(KeyCode.D))
         {
-            if (Input.GetKeyUp(KeyCode.D))
-            {
-
-            }
+            animator.SetBool("IsRunningRight", true);
+        }
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            animator.SetBool("IsRunningRight", false);
         }
     }
 }
