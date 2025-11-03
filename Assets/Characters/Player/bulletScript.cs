896654912c714bcf6ad47b61718488fa2b880ac6
eldar130 <veldarv@gmail.com>
2025-11-03 21:03:13 +0000

    bullet destroyed on collision
    the game object bullet will be destroyed when colliding with walls and maze

--- Assets/Characters/Player/bulletScript.cs
+++ Assets/Characters/Player/bulletScript.cs
@@ -6,10 +6,17 @@ public class bulletScript : MonoBehaviour
     private float lookAngle;
     [SerializeField] private GameObject bullet1;
 
-    void Update()
+    void Start()
     {
         lookDirection = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
         lookAngle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(0f, 0f, lookAngle);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "walls" || collision.gameObject.name == "maze")
+        {
+            Destroy(gameObject);
+        }
     }
 }
