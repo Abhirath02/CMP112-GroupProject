133c1726729a24879d7725525802a1b458c25bc1
eldar130 <veldarv@gmail.com>
2025-11-10 21:27:36 +0000

    text and health bar bug fix
    text and health are pined to the position of the map

--- Assets/In game items/gun/gun1Script.cs
+++ Assets/In game items/gun/gun1Script.cs
@@ -13,15 +13,13 @@ public class gun1Script : MonoBehaviour
         textMesh = GameObject.Find("pickUpText").GetComponent<TextMeshProUGUI>();
         textMesh.gameObject.SetActive(false);
     }
-    
+
     //check if player overlaping gun to show text
     void OnTriggerEnter2D(Collider2D gun1)
     {
         if (gun1.CompareTag("Player"))
         {
             textMesh.gameObject.SetActive(true);
-            RectTransform rectTransform = textMesh.GetComponent<RectTransform>();
-            rectTransform.anchoredPosition = new Vector2(100f, -180f);
         }
     }
     void OnTriggerExit2D(Collider2D gun1)
